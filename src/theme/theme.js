ad3dc7627b08b98a94a489ac323f1396bd90ff8b
Paz Spera <mspera03@gmail.com>
2025-04-15 21:14:59 -0300

    Agregado theme con colores de marca

---
+++ src/theme/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#242B74",
+    }, 
+    secondary: {
+      main: "#7EBDC2",
+    }
+  }
+})
