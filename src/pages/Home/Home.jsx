63e9fe870b743cc4d3afaa944cf254ea9d4768de
Paz Spera <mspera03@gmail.com>
2025-04-19 17:24:07 -0300

    Agregadas las secciones a la página de inicio

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
-
+import Hero from "../../sections/Hero/Hero";
+import PorQueElegirnos from "../../sections/PorQueElegirnos/PorQueElegirnos";
+import NuestrosProductos from "../../sections/NuestrosProductos/NuestrosProductos";
+import ProductosSector from "../../sections/ProductosSector/ProductosSector";
+import QuienesSomos from "../../sections/QuienesSomos/QuienesSomos";
+import NuestrosClientes from "../../sections/NuestrosClientes/NuestrosClientes";
+import Contacto from "../../sections/Contacto/Contacto";
 
 export default function Home() {
 
   return (
     <>
       <h1>Home Page</h1>
+      <Hero></Hero>
+      <PorQueElegirnos></PorQueElegirnos>
+      <NuestrosProductos></NuestrosProductos>
+      <ProductosSector></ProductosSector>
+      <QuienesSomos></QuienesSomos>
+      <NuestrosClientes></NuestrosClientes>
+      <Contacto></Contacto>
     </>
   )
 }
