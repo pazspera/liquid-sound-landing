a720692d449ba04a8b5205ef835ae2101e419e91
Paz Spera <mspera03@gmail.com>
2025-04-23 01:27:34 -0300

    Prueba layout Contacto. La grilla y el contenedor no se están comportando como esperaba, a revisar

--- src/sections/Contacto/Contacto.jsx
+++ src/sections/Contacto/Contacto.jsx
@@ -5,15 +5,15 @@ export default function Contacto() {
 
   return (
     <>
-      <Container maxWidth="xl" className={styles.contactContainer}>
-        <Grid container rowSpacing={2}>
+      <Container maxWidth="xl" className={styles.contactContainer} sx={{ display: "block" }}>
+        <Grid container spacing={2}>
           <Grid item xs={12}>
             <Typography variant="h3" gutterBottom>Contacto</Typography>
           </Grid>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={6} sx={{ border: '1px solid', p: 2 }}>
             <p>Contacto</p>
           </Grid>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={6} sx={{ border: '1px solid', p: 2 }}>
             <p>Contacto</p>
           </Grid>
         </Grid>
