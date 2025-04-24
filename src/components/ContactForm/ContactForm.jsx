9abc3568b3c34e131b9b8c4417e931b1cc6ec10e
Paz Spera <mspera03@gmail.com>
2025-04-24 02:07:31 -0300

    Retoques a layout ContactForm para que ajuste el tamaño un poco más pequeño en lg

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,22 +7,22 @@ export default function ContactForm() {
     <>
       <form>
         <Grid container rowSpacing={2} columnSpacing={2}>
-          <Grid size={{ xs: 12, md: 6 }}>
+          <Grid item size={{ xs: 12, md: 6, lg: 5 }} offset={{ lg: 1 }}>
             <TextField variant="filled" label="Nombre" type="text" required fullWidth />
           </Grid>
-          <Grid size={{ xs: 12, md: 6 }}>
+          <Grid item size={{ xs: 12, md: 6, lg: 5 }}>
             <TextField variant="filled" label="Empresa" type="text" fullWidth />
           </Grid>
-          <Grid size={{ xs: 12, md: 6 }}>
+          <Grid item size={{ xs: 12, md: 6, lg: 5}} offset={{ lg: 1 }}>
             <TextField variant="filled" label="Teléfono" type="tel" required fullWidth />
           </Grid>
-          <Grid size={{ xs: 12, md: 6 }}>
+          <Grid item size={{ xs: 12, md: 6, lg: 5 }}>
             <TextField variant="filled" label="Email" type="email" required fullWidth />
           </Grid>
-          <Grid size={{ xs: 12 }}>
+          <Grid item size={{ xs: 12, lg: 11 }} offset={{ lg: 1 }}>
             <FormControlLabel control={ <Checkbox /> } label="Suscribirme al newsletter" />
           </Grid>
-          <Grid size={{ xs: 12, md: 10 }} offset={{ xs: 0, md: 1 }}>
+          <Grid item size={{ xs: 12, md: 10, lg: 6 }} offset={{ xs: 0, md: 1, lg: 3 }}>
             <Button variant="contained" fullWidth>Enviar</Button>
           </Grid> 
         </Grid>
