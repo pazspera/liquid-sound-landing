1a45683ff37c5bc67f944eef0b7c8fe3ac8334ec
Paz Spera <mspera03@gmail.com>
2025-04-25 14:55:35 -0300

    Agregada función netlify para mandar correo con Resend

---
+++ netlify/functions/contact.js
@@ -0,0 +1,35 @@
+import { Resend } from "resend";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function handler(event) {
+  if(event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method not found" };
+  }
+
+  const data = JSON.parse(event.body);
+  const { nombre, empresa, telefono, email, newsletter } = data;
+
+  try {
+    const response = await resend.emails.send({
+      from: "Formulario <no-reply@liquidsound.com.ar>",
+      to: "spera.paz@gmail.com",
+      subject: "Mensaje formulario landing",
+      text: `Nombre: ${nombre} - Empresa: ${empresa} - Teléfono: ${telefono} - Email: ${email} - Newsletter: ${newsletter ? "Sí" : "No"}`.trim();
+    })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, id: response.id });
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message });
+    }
+  }
+
+}
