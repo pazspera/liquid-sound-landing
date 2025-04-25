3314ed095a1693e52d236d3a01e46890cb96cb7a
Paz Spera <mspera03@gmail.com>
2025-04-25 11:24:30 -0300

    Base de react-hook-form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,32 +1,66 @@
 import { Grid, TextField, Checkbox, FormControlLabel, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 export default function ContactForm() {
-
+  const { register, handleSubmit } = useForm();
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit((data)=> {
+        console.log(data)
+      })}>
         <Grid container rowSpacing={2} columnSpacing={2}>
           <Grid item size={{ xs: 12, md: 6, lg: 5 }} offset={{ lg: 1 }}>
-            <TextField variant="filled" label="Nombre" type="text" required fullWidth />
+            <TextField 
+              variant="filled" 
+              label="Nombre" 
+              type="text" 
+              required
+              fullWidth 
+              {...register("nombre")} 
+            />
           </Grid>
           <Grid item size={{ xs: 12, md: 6, lg: 5 }}>
-            <TextField variant="filled" label="Empresa" type="text" fullWidth />
+            <TextField 
+              variant="filled" 
+              label="Empresa" 
+              type="text" 
+              fullWidth
+              {...register("empresa")} 
+            />
           </Grid>
-          <Grid item size={{ xs: 12, md: 6, lg: 5}} offset={{ lg: 1 }}>
-            <TextField variant="filled" label="Teléfono" type="tel" required fullWidth />
+          <Grid item size={{ xs: 12, md: 6, lg: 5 }} offset={{ lg: 1 }}>
+            <TextField 
+              variant="filled" 
+              label="Teléfono" 
+              type="tel" 
+              required 
+              fullWidth 
+              {...register("telefono")} 
+            />
           </Grid>
           <Grid item size={{ xs: 12, md: 6, lg: 5 }}>
-            <TextField variant="filled" label="Email" type="email" required fullWidth />
+            <TextField 
+              variant="filled" 
+              label="Email" 
+              type="email" 
+              required 
+              fullWidth 
+              {...register("email")}
+            />
           </Grid>
           <Grid item size={{ xs: 12, lg: 11 }} offset={{ lg: 1 }}>
-            <FormControlLabel control={ <Checkbox /> } label="Suscribirme al newsletter" />
+            <FormControlLabel 
+              control={<Checkbox />} 
+              label="Suscribirme al newsletter" 
+              {...register("newsletter")}
+            />
           </Grid>
           <Grid item size={{ xs: 12, md: 10, lg: 6 }} offset={{ xs: 0, md: 1, lg: 3 }}>
-            <Button variant="contained" fullWidth>Enviar</Button>
-          </Grid> 
+            <Button variant="contained" type="submit" fullWidth>Enviar</Button>
+          </Grid>
         </Grid>
-      </form> 
+      </form>
     </>
   )
 }
