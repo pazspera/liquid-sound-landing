cd92baf2e0b3af4bb80e4c0ab97e3a4b4378003f
Paz Spera <mspera03@gmail.com>
2025-04-30 20:15:42 -0300

    Layout final de sección PorQueElegirnos

--- src/components/CardHighlight/CardHighlight.jsx
+++ src/components/CardHighlight/CardHighlight.jsx
@@ -1,35 +1,51 @@
 import { useTheme } from "@mui/material/styles";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Box, CardMedia } from "@mui/material";
 
 
 export default function CardHighlight({card}) {
   const theme = useTheme();
-  
+
   return (
     <>
       <Grid
         item
         size={{ xs: 12, md: 6, lg: 4 }}
+        offset={{ md: card.lastCard ? 3 : 0, lg: 0 }}
         sx={{
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center"
+          justifyContent: "center",
+          padding: "0 12px",
+          marginBottom: { xs: "36px", lg: "0" }
         }}
       >
-        <img src={card.imgSrc} alt={card.imgAlt} />
-        <Typography
-          variant="h6"
-          component="h3"
-          gutterBottom
-          sx={{ color: theme.palette.text.secondary }}
-        >
-          {card.title}
-        </Typography>
-        <Typography
-          sx={{ textAlign: "center" }}
-        >
-          {card.text}
-        </Typography>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          maxWidth: "350px",
+          width: "100%",  
+        }}>
+          <CardMedia 
+            component="img"
+            src={card.imgSrc} 
+            alt={card.imgAlt} 
+            sx={{ maxWidth: { xs: "96px", lg: "120px" }}}
+          />
+          <Typography
+            variant="h6"
+            component="h3"
+            gutterBottom
+            sx={{ color: theme.palette.text.secondary, marginTop: "8px", textAlign: "center" }}
+          >
+            {card.title}
+          </Typography>
+          <Typography
+            sx={{ textAlign: "center" }}
+            variant="body2"
+          >
+            {card.text}
+          </Typography>
+        </Box>
       </Grid>
     </>
   )
--- src/sections/PorQueElegirnos/PorQueElegirnos.jsx
+++ src/sections/PorQueElegirnos/PorQueElegirnos.jsx
@@ -11,21 +11,24 @@ export default function  PorQueElegirnos() {
       title: "Reducción de costos operativos",
       text: "Nuestros productos ayudan a reducir costos operativos al minimizar desperdicios y optimizar el uso de recursos en procesos de limpieza.",
       imgSrc: "/img/reduccion-costos.svg",
-      imgAlt: "Mano depositando monedas en un frasco"
+      imgAlt: "Mano depositando monedas en un frasco",
+      lastCard: false
     },
     {
       id: 2,
       title: "Flexibilidad en pedidos",
       text: "Adaptamos pedidos a las necesidades específicas de cada cliente, permitiendo soluciones personalizadas que se ajustan a demandas variables del mercado.",
       imgSrc: "/img/flexibilidad-pedidos.svg",
-      imgAlt: "Carrito transportando paquetes"
+      imgAlt: "Carrito transportando paquetes",
+      lastCard: false
     },
     {
       id: 3,
       title: "Cumplimiento normativo estricto",
       text: "Garantizamos el cumplimiento de normativas vigentes, proporcionando productos que facilitan auditorías y reducen riesgos operativos en entornos altamente regulados.",
       imgSrc: "/img/cumplimiento-normativo.svg",
-      imgAlt: "Documento con sello de aprobación"
+      imgAlt: "Documento con sello de aprobación",
+      lastCard: true
     }
   ]
 
