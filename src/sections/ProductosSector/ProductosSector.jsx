856d79c7fda611440722ee490be716669458234f
Paz Spera <mspera03@gmail.com>
2025-05-01 19:01:48 -0300

    Títulos y gradiente de ProductosSector

--- src/sections/ProductosSector/ProductosSector.jsx
+++ src/sections/ProductosSector/ProductosSector.jsx
@@ -1,34 +1,35 @@
+import styles from "./ProductosSector.module.css";
 import { useTheme } from "@mui/material/styles";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, Box } from "@mui/material";
 
 export default function ProductosSector() {
   const theme = useTheme();
 
   return (
     <>
-      <Container maxWidth="lg" sx={{ py: theme.layout.sectionPadding }}>
-        <Grid container>
-          <Grid size={{ xs: 12 }}>
-            <Typography 
-              variant="h3"
-              component="h2"
-              gutterBottom
-              sx={{
-                padding: theme.layout.titlePadding,
-                color: theme.palette.text.secondary,
-                textAlign: "center"
-              }}
-            >
-              Productos diseñados para su sector
-            </Typography>
-            <Typography
-              component="p"
-            >
-              Adaptamos nuestras soluciones a los estándares de cada industria para garantizar espacios seguros y libres de contaminación.
-            </Typography>
+      <Box className={styles.background}>
+        <Container maxWidth="lg" sx={{ py: theme.layout.sectionPadding }}>
+          <Grid container>
+            <Grid size={{ xs: 12 }}>
+              <Typography 
+                variant="h3"
+                component="h2"
+                gutterBottom
+                sx={{
+                  padding: theme.layout.titlePadding,
+                  color: theme.palette.text.secondary,
+                  textAlign: "center"
+                }}
+              >
+                Productos diseñados para su sector
+              </Typography>
+              <Typography component="p" variant="body3" sx={{ textAlign: "center" }}>
+                Adaptamos nuestras soluciones a los estándares de cada industria para garantizar espacios seguros y libres de contaminación.
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
     </>
   )
 }
