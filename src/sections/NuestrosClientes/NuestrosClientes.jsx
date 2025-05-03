47feb8f45f78d6c1345760e2867f7129a84e7e84
Paz Spera <mspera03@gmail.com>
2025-05-03 02:06:25 -0300

    Agregada configuración splide

--- src/sections/NuestrosClientes/NuestrosClientes.jsx
+++ src/sections/NuestrosClientes/NuestrosClientes.jsx
@@ -2,7 +2,7 @@ import styles from "./NuestrosClientes.module.css";
 import { Container, Grid, Typography, Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 
 export default function NuestrosClientes() {
   const theme = useTheme();
@@ -25,7 +25,7 @@ export default function NuestrosClientes() {
               Nuestros clientes
             </Typography>
           </Grid>
-          <Grid size={{ xs: 12 }}>
+          <Grid size={{ xs: 12}} sx={{ py: 3 }}>
               <Splide
                 options={{
                   type: "loop",
@@ -36,28 +36,37 @@ export default function NuestrosClientes() {
                   snap: true,
                   arrows: false,
                   interval: 3000,
-                  pauseOnHover: true,
-                  pauseOnFocus: true,
                   autoStart: true,
                   autoScroll: {
                     speed: 1,
+                    pauseOnHover: true,
+                    pauseOnFocus: true,
                   },
+                  breakpoints: {
+                    300: {
+                      perPage: 1
+                    },
+                    400: {
+                      perPage: 1
+                    }
+                  }
                 }}
+                extensions={{AutoScroll}}
               >
                 <SplideSlide>
-                  <img src="/img/slider-nestle.jpg" alt="Nestle" />
+                  <img src="/img/slider-nestle.jpg" alt="Nestle" className={styles.sliderLogo} />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src="/img/slider-diagnostico-maipu.jpg" alt="Diagnóstico Maipu" />
+                  <img src="/img/slider-diagnostico-maipu.jpg" alt="Diagnóstico Maipu" className={styles.sliderLogo} />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src="/img/slider-lab-medicina.jpg" alt="Lab Medicina" />
+                  <img src="/img/slider-lab-medicina.jpg" alt="Lab Medicina" className={styles.sliderLogo} />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src="/img/slider-casara.jpg" alt="Casará" />
+                  <img src="/img/slider-casara.jpg" alt="Casará" className={styles.sliderLogo} />
                 </SplideSlide>
                 <SplideSlide>
-                  <img src="/img/slider-pelo-mania.jpg" alt="Pelo mania" />
+                  <img src="/img/slider-pelo-mania.jpg" alt="Pelo mania" className={styles.sliderLogo} />
                 </SplideSlide>
               </Splide>
           </Grid>
