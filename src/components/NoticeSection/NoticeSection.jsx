22df11c59b6b47973bae38643bdaec276c086c94
Paz Spera <mspera03@gmail.com>
2025-05-03 23:57:04 -0300

    Creado componente NoticeSection para usar en thank you page y 404. Agregado a thank you page

---
+++ src/components/NoticeSection/NoticeSection.jsx
@@ -0,0 +1,34 @@
+import { useTheme } from "@mui/material/styles";
+import { Paper, Grid, Typography, Button, Container } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export default function NoticeSection({title, message, secondaryMessage}) {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Container maxWidth="lg" sx={{ minHeight: { xs: "60vh", md: "80vh" }, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+        <Paper elevation={3} sx={{ p: { xs: 4 }, backgroundColor: theme.palette.primary.main, color: theme.palette.common.white, maxWidth: { xs: "100%", md: "80vw", lg: "60vw" } }}>
+          <Grid container rowSpacing={2}>
+            <Grid size={{ xs: 12 }} sx={{ textAlign: "center" }}>
+              <Typography variant="h3" component="h1">
+                {title}
+              </Typography>
+            </Grid>
+            <Grid size={{ xs: 12 }} sx={{ textAlign: "center", mb: 3 }}>
+              <Typography variant="subtitle1" component="p">
+                {message}
+              </Typography>
+              <Typography variant="subtitle1" component="p">
+                {secondaryMessage}
+              </Typography>
+            </Grid>
+            <Grid size={{ xs: 12 }} sx={{ textAlign: "center" }}> 
+              <Button variant="contained" component={Link} to="/">Volver al inicio</Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    </>
+  )
+}
--- src/pages/ThankYou/ThankYou.jsx
+++ src/pages/ThankYou/ThankYou.jsx
@@ -1,11 +1,19 @@
 import useDocumentTitle from "../../hooks/useDocumentTitle"
+import { useTheme } from "@mui/material/styles";
+import { Container } from "@mui/material";
+import NoticeSection from "../../components/NoticeSection/NoticeSection";
 
 export default function ThankYou() {
+  const theme = useTheme();
   useDocumentTitle("Gracias por contactarnos - Liquid Sound"); 
 
   return (
     <>
-      <h1>Thank you</h1>
+      <NoticeSection 
+        title={"¡Gracias por contactarnos!"} 
+        message={"Su mensaje fue enviado."} 
+        secondaryMessage={"Nuestro equipo se pondrá en contacto a la brevedad."} 
+      />
     </>
   )
 }
